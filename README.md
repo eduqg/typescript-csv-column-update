6187fab286d32ce8c9725780cded5a2b5c19622f
eduqg <eduardoqgomes@gmail.com>
2023-07-01 19:22:50 -0300

    Update csv column

--- README.md
+++ README.md
@@ -1,1 +1,37 @@
-# javascript-csv-column-compare
+# typescript-csv-column-update
+
+Simple project to help update or compare csv columns based on function.
+
+## Getting started
+
+Install dependencies
+
+```console
+yarn
+```
+
+or
+
+```console
+npm install
+```
+
+Run main.ts file
+
+```console
+yarn start
+```
+
+or
+
+```console
+npm run start
+```
+
+## References
+
+[Slugify function](https://gist.github.com/codeguy/6684588)
+
+## License
+
+[MIT](https://choosealicense.com/licenses/mit/)
--- main.ts
+++ main.ts
@@ -1,4 +1,59 @@
-// const fs = require("fs");
-// const { parse } = require("csv-parse");
+import fs from "fs";
+import { parse } from "csv-parse";
 
-console.log('ola')
+interface IUpdateSlugRows {
+  input: string;
+  output: string;
+}
+interface IError {
+  message: string;
+}
+
+function slugify(text: string) {
+  return text
+    .toString()
+    .normalize("NFD")
+    .replace(/[\u0300-\u036f]/g, "")
+    .toLowerCase()
+    .trim()
+    .replace(/\s+/g, "-")
+    .replace(/[^\w\-]+/g, "")
+    .replace(/\-\-+/g, "-");
+}
+
+function joinResults(results: string[][]) {
+  const header = ["id, name, slug"];
+  const rows = results.map((item) => `${item[0]}, ${item[1]}, ${item[2]}`);
+  return header.concat(rows).join("\n");
+}
+
+function writeToCSVFile(results: Array<string[]>, output: string) {
+  fs.writeFile(output, joinResults(results), (err: any) => {
+    if (err) {
+      console.log("Error writing to csv file", err);
+    } else {
+      console.log(`Saved as ${output}`);
+    }
+  });
+}
+
+function updateSlugRows({ input, output }: IUpdateSlugRows) {
+  let results: Array<string[]> = [];
+
+  fs.createReadStream(`./${input}`)
+    .pipe(parse({ delimiter: ",", from_line: 2 }))
+    .on("data", function (row: string[]) {
+      row[2] = slugify(row[1]);
+      results.push(row);
+    })
+    .on("end", function () {
+      writeToCSVFile(results, output);
+    })
+    .on("error", function (error: IError) {
+      console.log(error.message);
+    });
+}
+
+updateSlugRows({ input: "input.csv", output: "output.csv" });
+
+console.log("Finished!");
